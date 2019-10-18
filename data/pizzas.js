3d7fb9f3d571b7764e9f774bd4f729606d240c9e
adam <adam@harpur.io>
2019-10-18 20:09:47 +0100

    feat: pizza store usage

--- data/pizzas.js
+++ data/pizzas.js
@@ -1,42 +1,61 @@
-const { filter, orderBy, values } = require("lodash");
+const { filter, orderBy, values } = require("lodash")
 
-const Pizza = require("../models/pizza");
-const ImageStore = require("../lib/imageStore");
-
-const pizzas = {};
+const Pizza = require("../models/pizza")
+const ImageStore = require("../lib/imageStore")
+const PizzaStore = require("./pizzaStore")
 
 async function create(name, toppings, img, username) {
-  const imgUrl = await ImageStore.save(name.replace(/ /g, "-"), img);
-  const pizza = new Pizza(name, toppings, imgUrl, username);
-  pizzas[pizza.id] = pizza;
-  return pizza;
+  const imgUrl = await ImageStore.save(name.replace(/ /g, "-"), img)
+  const pizza = new Pizza(name, toppings, imgUrl, username)
+  return PizzaStore.create(encodePizza(pizza))
 }
 
 // for mocks that don't need pizza images saved
 function batchImport(name, toppings, imgUrl, username) {
-  const pizza = new Pizza(name, toppings, imgUrl, username);
-  pizzas[pizza.id] = pizza;
+  const pizza = new Pizza(name, toppings, imgUrl, username)
+  PizzaStore.create(encodePizza(pizza))
 }
 
 async function getForUser(username) {
-  const userPizzas = filter(pizzas, pizza => pizza.username === username);
-  return userPizzas;
+  return PizzaStore.findAll({
+    where: {
+      username
+    },
+    raw: true
+  }).then(decodePizzas)
 }
 
 async function getRecent() {
-  const recentPizzas = orderBy(pizzas, ["created"], ["desc"]);
-  return values(recentPizzas).splice(0, 5);
+  return PizzaStore.findAll({
+    order: [["created", "DESC"]],
+    limit: 4,
+    raw: true
+  }).then(decodePizzas)
 }
 
 async function get(pizzaId) {
-  if (!pizzas[pizzaId]) throw new Error("Pizza not found");
-  return pizzas[pizzaId];
+  return PizzaStore.findOne({
+    where: {
+      id: pizzaId
+    },
+    raw: true
+  }).then(decodePizza)
 }
 
+function encodePizza(pizza) {
+  return { ...pizza, toppings: JSON.stringify(pizza.toppings) }
+}
+function decodePizza(pizza) {
+  return { ...pizza, toppings: JSON.parse(pizza.toppings) }
+}
+
+function decodePizzas(pizzas) {
+  return pizzas.map(decodePizza)
+}
 module.exports = {
   batchImport,
   create,
   get,
   getForUser,
   getRecent
-};
+}
